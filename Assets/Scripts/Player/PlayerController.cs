11ebc1255dfbea57fd53bea73565dbbb00d09198
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2024-02-12 18:55:29 +0200

    first commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private MainInput controls;
+
+    void Awake()
+    {
+        controls = new MainInput();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 moveInput = controls.Player.Move.ReadValue<Vector2>();
+        Vector2 movement = moveInput * 5 * Time.fixedDeltaTime;
+        body.MovePosition(body.position + movement);
+    }
+}
