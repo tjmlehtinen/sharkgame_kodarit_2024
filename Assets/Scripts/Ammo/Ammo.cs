45a59e7283a6a30164cc18a74f12786204756643
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2024-02-12 19:30:57 +0200

    ammo moving

---
+++ Assets/Scripts/Ammo/Ammo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    private float currentSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.up * -1 * currentSpeed * Time.fixedDeltaTime);
+    }
+}
